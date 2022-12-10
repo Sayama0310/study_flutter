15d5ba20557ecb3fdffa551cfcc036f00dac709e
Sayama0310 <4696monochrome@ezweb.ne.jp>
2022-12-10 18:04:58 +0900

    initial commit: add some page; Unorganized.

---
+++ lib/list_view_sample.dart
@@ -0,0 +1,16 @@
+// ListViewの使い方について調査用にサンプルページを実装
+import 'package:flutter/material.dart';
+
+class ListViewSamplePage extends StatefulWidget {
+  const ListViewSamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewSamplePage> createState() => _ListViewSamplePageState();
+}
+
+class _ListViewSamplePageState extends State<ListViewSamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
