5a69f8e3da8343d1b6c465aadfeeb51ccf76567d
Sayama0310 <4696monochrome@ezweb.ne.jp>
2022-12-22 21:45:49 +0900

    [Comment] コメント文の整理

--- lib/boring_to_beautiful/main.dart
+++ lib/boring_to_beautiful/main.dart
@@ -2,6 +2,17 @@ import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:universal_platform/universal_platform.dart';
 
+// https://codelabs.developers.google.com/codelabs/flutter-boring-to-beautiful#0
+// https://github.com/flutter/codelabs/tree/master
+// FlutterのCodelabsというリポジトリにサンプルアプリがたくさんあったので、こちらを実装してみた。
+// 以下の機能の学習。
+//   * プラットフォーム横断的に美しく使いやすいアプリの書き方。
+//   * アプリ内のテキストがユーザーエクスペリエンスを高めるためのデザイン方法。
+//   * 正しい色の選び方、ウィジェットのカスタマイズ、独自のテーマの作り方、そして素早く簡単にダークモードを実装する方法。
+//   * クロスプラットフォーム対応のアプリを構築する方法。
+//   * どんなスクリーンでも良く見えるアプリの構築の仕方。
+//   * 動きをつけてポップにする方法。
+
 Future setDesktopWindow() async {
   await DesktopWindow.setMinWindowSize(const Size(400, 400));
   await DesktopWindow.setWindowSize(const Size(1300, 900));
@@ -11,6 +22,7 @@ void main() {
   // TODO : これは何をやっているのか？
   WidgetsFlutterBinding.ensureInitialized();
 
+  // もしデスクトップ向けに起動していた時は、Windowの最小幅、最小高さなどを設定する。
   if (UniversalPlatform.isDesktop) {
     setDesktopWindow();
   }
