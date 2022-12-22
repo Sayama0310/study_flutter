fa667f0f67fb049765f6f397fba01711a3bb2212
Sayama0310 <4696monochrome@ezweb.ne.jp>
2022-12-22 21:33:14 +0900

    [Refactor] 階層を整理

---
+++ lib/listview_sample/src/list_view_sample.dart
@@ -0,0 +1,62 @@
+// ListViewの使い方について調査用にサンプルページを実装
+import 'package:flutter/material.dart';
+
+class ListViewSamplePage extends StatefulWidget {
+  const ListViewSamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewSamplePage> createState() => _ListViewSamplePageState();
+}
+
+final List<String> entries = <String>['A', 'B', 'C'];
+final List<int> colorCodes = <int>[600, 500, 100];
+
+class _ListViewSamplePageState extends State<ListViewSamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Sample'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: ListView(
+              padding: const EdgeInsets.all(10),
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('Entry C')),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 50,
+                  color: Colors.amber[colorCodes[index]],
+                  child: Center(child: Text('Entry ${entries[index]}')),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
