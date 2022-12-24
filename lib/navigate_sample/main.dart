c1261470c226dfaa6db3f9edce2440df7ce69c73
Sayama0310 <4696monochrome@ezweb.ne.jp>
2022-12-24 19:18:28 +0900

    [Feat-Add] ホームページを作成

---
+++ lib/navigate_sample/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:study_flutter/navigate_sample/src/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        // Add the 5 lines from here...
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
